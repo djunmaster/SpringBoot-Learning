265b4ae5bab57810bd487b5e7a52f4ed63ea866c
liuyd <1003478061@qq.com>
2024-08-03 22:51:52 +0800

    initial git project

---
+++ src/test/java/com/pilot/mygit/MyGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilot.mygit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
