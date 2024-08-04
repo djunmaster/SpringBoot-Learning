485278525b256e069e6d78afba9bb15692911e97
liuyd <1003478061@qq.com>
2024-08-04 08:22:06 +0800

    develop add Test1.java file

---
+++ src/main/java/com/pilot/mygit/controller/Test1.java
@@ -0,0 +1,10 @@
+package com.pilot.mygit.controller;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world Test1");
+        System.out.println("hello world Test1");
+        System.out.println("hello world Test1");
+    }
+}
